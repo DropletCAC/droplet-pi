8b45987b8bb5e1a49119689ef3336715950e4a1b
TanujKS <tanujsiripurapu@gmail.com>
2023-10-08 19:59:28 -0700

    Initial Commit

---
+++ bucket/bucket.py
@@ -0,0 +1,100 @@
+from flask import Flask, request 
+import json
+import RPi.GPIO as GPIO
+import time, sys, os
+import threading 
+import requests
+
+app = Flask(__name__)
+
+
+class Bucket:
+    def __init__(self, user_id, bucket, base_dim, height_dim):
+        self.user_id = user_id
+        self.bucket = bucket 
+        self.base_dim = base_dim
+        self.height_dim = height_dim
+        
+        
+    def send(self, gal):
+        requests.post(f"https://bengal-sought-bedbug.ngrok-free.app/setBucketLevel?user={self.user_id}&bucket={self.bucket}&volume={gal}")
+
+
+    def start(self):
+        GPIO.setmode(GPIO.BCM)
+
+        TRIG = 2
+        ECHO = 3
+        i=0
+
+        GPIO.setup(TRIG ,GPIO.OUT)
+        GPIO.setup(ECHO,GPIO.IN)
+
+        GPIO.output(TRIG, False)
+        print("Starting.....")
+        time.sleep(2)
+
+        while True:
+            GPIO.output(TRIG, True)
+            time.sleep(0.00001)
+            GPIO.output(TRIG, False)
+
+            while GPIO.input(ECHO)==0:
+                pulse_start = time.time()
+
+            while GPIO.input(ECHO)==1:
+                pulse_stop = time.time()
+
+            pulse_time = pulse_stop - pulse_start
+
+            distance = pulse_time * 17150
+            print(round(distance, 2));
+
+            time.sleep(1)
+
+            print("Distance (cm)", distance)
+            distance /= 2.54
+            print("Distance (in)", distance)
+            
+            print("Height of Water (in)", self.height_dim - distance)
+            
+            water_height = self.height_dim - distance 
+            vol = water_height * self.base_dim
+            
+            print("Vol (Inches^3)", vol)
+            vol /= 231
+            
+            print("Vol (gal)", vol)
+            
+            self.send(vol)
+
+
+
+@app.route('/setup', methods=['GET'])
+def setup():
+    user = request.args.get('user')
+    bucket = request.args.get('bucket')
+    base_dim = request.args.get('base_dim')
+    height_dim = request.args.get('height_dim')
+
+    print("Setting up and pairing to user", user)
+    
+    with open("user.json", "w+") as file:
+        file.write(json.dumps({"user": user, "bucket": bucket, "base_dim": base_dim, "height_dim": height_dim}))
+    
+    bucket = Bucket(user, bucket)
+    func = threading.Thread(target=bucket.start)
+    func.start()
+
+    return json.dumps({"success": True, "user": user})
+
+
+if __name__ == "__main__":
+    if os.path.exists("user.json"):
+        with open("user.json") as file:
+            user_data = json.load(file)
+            print(user_data)
+            bucket = Bucket(user_data['user'], user_data['bucket'], user_data['base_dim'], user_data['height_dim'])
+            bucket.start()
+    else:
+        app.run(host="0.0.0.0", port=8080)
---
+++ meter/meter.py
@@ -0,0 +1,102 @@
+from flask import Flask, request 
+import json
+import RPi.GPIO as GPIO
+import time, sys, os
+from datetime import datetime 
+import threading 
+import numpy as np
+import requests
+
+app = Flask(__name__)
+
+
+class Meter:
+    def __init__(self, user_id, section):
+        self.count = 0
+        self.start_counter = 0
+        self.user_id = user_id
+        self.section = section 
+        self.total = 0
+        
+    def send(self, gal):
+        requests.post(f"https://bengal-sought-bedbug.ngrok-free.app/setCurrentUsage?user={self.user_id}&section={self.section}&usage={gal}")
+
+    def countPulse(self, channel):
+        if self.start_counter == 1:
+            self.count = self.count+1
+
+
+    def start(self):
+        FLOW_SENSOR_GPIO = 13
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(FLOW_SENSOR_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        
+
+        GPIO.add_event_detect(FLOW_SENSOR_GPIO, GPIO.FALLING, callback=self.countPulse)
+
+        while True:
+            try:                
+                # start_counter = 1
+                # print("Starting counter...")
+                # time.sleep(1)
+                # print("Calculating")
+                # start_counter = 0
+                # flow = (self.count / 7.5) # Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+                # print(self.count, flow)
+                # gal = flow / 3.785
+
+                # message = f"Flow Rate: {flow} L/min | {gal} gal/min"
+                # print(message)
+
+                # self.count = 0
+                # time.sleep(5)
+                x = 0
+                flows = []
+                while (x < 60):
+                    self.start_counter = 1
+                    time.sleep(1)
+                    self.start_counter = 0
+                    flows.append(self.count / 7.5 / 3.785) # Pulse frequency (Hz) = 7.5Q, Q is flow rate in L/min.
+                    print("The flow is: %.3f gal/min" % (flows[-1]), x)
+                    self.count = 0
+                    if flows[-1] > 0:
+                        x += 1
+
+                flows = np.array(flows)
+                avg_flow = np.mean(flows)
+                print("AVG FLOW", avg_flow)
+                self.send(avg_flow)
+                
+            except KeyboardInterrupt:
+                print('\nkeyboard interrupt!')
+                GPIO.cleanup()
+                sys.exit()
+
+
+@app.route('/setup', methods=['GET'])
+def setup():
+    user = request.args.get('user')
+    section = request.args.get('section')
+
+    print("Setting up and pairing to user", user)
+    
+    with open("user.json", "w+") as file:
+        file.write(json.dumps({"user": user, "section": section}))
+    
+    meter = Meter(user, section)
+    func = threading.Thread(target=meter.start)
+    func.start()
+
+    return json.dumps({"success": True, "user": user})
+
+
+if __name__ == "__main__":
+    if os.path.exists("user.json"):
+        with open("user.json") as file:
+            user_data = json.load(file)
+            print(user_data)
+            meter = Meter(user_data['user'], user_data['section'])
+            meter.start()
+    else:
+        app.run(host="0.0.0.0", port=8080)
